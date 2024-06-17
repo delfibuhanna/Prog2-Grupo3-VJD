e7aca7ea1785a02882de6b75f7c5e3cb307b22de
julimielnicki <jmielnicki@udesa.edu.ar>
2024-06-17 19:30:08 -0300

    comence el punto 3

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -9,12 +9,44 @@ const usuariosController ={
     res.render("profileEdit", {lista: data});
 },
   login: function(req,res){
-    return res.render("login");
+    return res.render("login");   
 },
   register: function(req,res){
     return res.render("register");
-}
+},
+loginUser: (req, res)=>{
+  let form = req.body;
+
+  let filtro = {
+      where: [{email: form.email}]
+  };
+
+  db.User.findOne(filtro)
+  .then((result) => {
+
+      if (result == null) return res.send("No existe el mail " +  form.email)
+      
+
+      let check = bcrypt.compareSync(form.Contrasenia, result.contrasenia);
+      if (check) {
+          req.session.user = result;
+
+          /* que lo guarde en cookie si el usuario lo tildo */
+          if (form.rememberme != undefined) {
+              res.cookie("userId", result.id, {maxAge: 1000 * 60 * 15});
+          }
+          return res.redirect("/users");
+      } else {
+          return res.send("La contraseña es incorrecta")
+      }
+      
+     
+
+  }).catch((err) => {
+      return console.log(err);
+  });}
 
 };
+
 
 module.exports= usuariosController;
